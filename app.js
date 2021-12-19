71d92745dfbd37e80607151a79a8f5d261210b8e
MadIIT <shridspace@gmail.com>
2021-12-20 02:10:20 +0530

    ''

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ app.use(express.static(path.join(__dirname, 'css')))
 img = path.join(__dirname, '/images/')
 
 arr = [
-    {brand:'Bisleri', size: '500ml', photo: 'https://5.imimg.com/data5/NJ/BN/QU/ANDROID-39606195/product-jpeg-500x500.jpeg',cost: '10'},
-    {brand:'Aquafina', size: '500ml', photo: 'https://5.imimg.com/data5/QQ/YK/TM/SELLER-98558878/aquafina-bottle-500-ml-500x500.jpg',cost: '10'},
-    {brand: 'Kinley', size: '500ml', photo: 'https://5.imimg.com/data5/QL/CF/HP/SELLER-7474209/500-ml-500x500.png',cost: '10'}
+    {brand:'Bisleri', size: '500ml', photo: '',cost: '10'},
+    {brand:'Aquafina', size: '500ml', photo: '',cost: '10'},
+    {brand: 'Kinley', size: '500ml', photo: '',cost: '10'}
 ]
 
 app.get('/', (req, res) => {
