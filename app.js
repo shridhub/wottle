8776027d9d950a196cd85b54635c98f0564708d5
MadIIT <shridspace@gmail.com>
2021-12-20 02:15:09 +0530

    ''

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path')
 viewsPath = path.join(__dirname, 'pages/views')
 partPaths = path.join(__dirname, '/pages/partials')
 
-const port = 3000 || process.env.PORT
+const port = process.env.PORT || 3000
 
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
